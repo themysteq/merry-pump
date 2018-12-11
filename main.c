86d04fdc30375e02882b73e9a844dde5a2282f15
themysteq <pawelsadlinki@gmail.com>
2018-12-11 20:46:45 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#define F_CPU 8000000
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+#include <util/atomic.h>
+
+
+
+int main(void) {
+
+    while(1){
+
+    }
+
+}
