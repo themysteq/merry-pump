c634e9f5cdb1d112a4c1e60608f4cd3b28ebc109
themysteq <pawelsadlinki@gmail.com>
2018-12-16 20:02:44 +0100

    Works! Tested with water

--- main.c
+++ main.c
@@ -1,38 +1,47 @@
 #define F_CPU 8000000
 #include <avr/io.h>
-#include <avr/interrupt.h>
+//#include <avr/interrupt.h>
 #include <stdio.h>
 #include <util/delay.h>
 #include <string.h>
-#include <util/atomic.h>
+//#include <util/atomic.h>
+#include <stdbool.h>
 
 #define IDLE_STATE 0x03
 #define WORKING_STATE 0x09
 #define FAIL_STATE 0x00
 #define PUMPING_SYSTEM_PORT PORTD
+#define SENSOR_PORT PIND
 #define PUMPING_SYSTEM_DDR DDRD
 #define INPUT_TANK_PIN PD5
 #define OUTPUT_TANK_PIN PD6
 #define PUMP_PIN PD7
 #define VALVE_PIN PD4
-#define FAIL_LED_PIN PB1
+#define FAIL_LED_PIN PB3
 #define WORKING_LED_PIN PB2
-#define IDLE_LED_PIN PB3
+#define IDLE_LED_PIN PB1
 #define LED_PORT PORTB
 #define LED_PORT_DDR DDRB
 
+volatile unsigned char current_state = IDLE_STATE;
 
-
-volatile unsigned char current_state = FAIL_STATE;
-
-void fail_led(void){
-    //TODO
+void fail_led_on(void){
+    LED_PORT = (1<<FAIL_LED_PIN);
 }
-void working_led(void){
-    //TODO
+void fail_led_off(void){
+    //LED_PORT = ~(1<<FAIL_LED_PIN);
 }
-void idle_led(void){
-    //TODO
+void working_led_on(void){
+    LED_PORT = (1<<WORKING_LED_PIN);
+}
+void working_led_off(void){
+   // LED_PORT = ~(1<<WORKING_LED_PIN);
+}
+void idle_led_on(void){
+    LED_PORT = (1<<IDLE_LED_PIN);
+}
+void idle_led_off(void){
+   // LED_PORT &= ~(1<<IDLE_LED_PIN);
 }
 void stop_pump(void){
     PUMPING_SYSTEM_PORT &= ~(1<<PUMP_PIN);
@@ -51,74 +60,95 @@ void open_valve(void){
 }
 
 unsigned char check_input_tank_empty(void){
-    //true if not empty
-    return (unsigned char)((PUMPING_SYSTEM_PORT)&(1<<INPUT_TANK_PIN));
+    //true if empty
+    return (unsigned char)0+(unsigned char)((SENSOR_PORT)&(1<<INPUT_TANK_PIN));
 }
 
-unsigned char check_output_tank_full(void){
-    //true if full
-    return (unsigned char)((PUMPING_SYSTEM_PORT)&(1<<OUTPUT_TANK_PIN));
+unsigned char check_output_tank_empty(void){
+    //true if empty
+    return (unsigned char)0+(unsigned char)((SENSOR_PORT)&(1<<OUTPUT_TANK_PIN));
 }
 
-unsigned char handle_fail_state(void){
+void handle_fail_state(void){
+    fail_led_on();
     stop_pump();
     close_valve();
-    fail_led();
-    if(check_input_tank_empty()) return FAIL_STATE;
-    if(!check_input_tank_empty()) return IDLE_STATE;
-
-    return FAIL_STATE;
+    if(check_input_tank_empty()) {
+        current_state = FAIL_STATE;
+    }
+    else {
+        current_state = IDLE_STATE;
+    }
 
 }
 
-unsigned char handle_idle_state(void){
+void handle_idle_state(void){
     stop_pump();
     close_valve();
-    idle_led();
-    if(check_input_tank_empty()) return FAIL_STATE;
-    if(check_output_tank_full()) return IDLE_STATE;
-    if(!check_output_tank_full()) return WORKING_STATE;
+    idle_led_on();
+    if(check_input_tank_empty()>0) {
+        current_state = FAIL_STATE;
+    }
+    else if(check_output_tank_empty())
+    {
+        current_state = WORKING_STATE;
+    }
+    else {
+        current_state = IDLE_STATE;
+    }
 
-    return IDLE_STATE;
  }
 
-unsigned char handle_working_state(void){
+void handle_working_state(void){
     open_valve();
     start_pump();
-    working_led();
-    if(check_output_tank_full()) return IDLE_STATE;
-    if(check_input_tank_empty()) return FAIL_STATE;
-    if(!check_output_tank_full()) return WORKING_STATE;
+    working_led_on();
+    if(check_input_tank_empty()) { current_state = FAIL_STATE;
+        return;
+    }
+    else if(check_output_tank_empty()){
+        current_state = WORKING_STATE;
+        return;
+    }
+    else {
+        current_state = IDLE_STATE;
+        return;
+    }
 
-    return IDLE_STATE;
+    //return current_state;
 }
 
 void init_system(void){
-    _delay_ms(4000);
-    PUMPING_SYSTEM_DDR &= (~(1<<OUTPUT_TANK_PIN))&(~(1<<INPUT_TANK_PIN)); //input 0
+    _delay_ms(1000);
+    LED_PORT_DDR |= (1<<FAIL_LED_PIN)|(1<<IDLE_LED_PIN)|(1<<WORKING_LED_PIN);
+    fail_led_on();
+    _delay_ms(1000);
+    working_led_on();
+    _delay_ms(1000);
+    idle_led_on();
+    _delay_ms(1000);
+    //PUMPING_SYSTEM_DDR &= (~(1<<OUTPUT_TANK_PIN))&(~(1<<INPUT_TANK_PIN)); //input 0
     PUMPING_SYSTEM_DDR |= (1<<PUMP_PIN)|(1<<VALVE_PIN); //output 1
     stop_pump();
     close_valve();
-    current_state = FAIL_STATE;
-    _delay_ms(4000);
+    current_state = IDLE_STATE;
+    _delay_ms(1000);
 }
 int main(void){
     init_system();
     while(1){
-        _delay_ms(100);
-        switch(current_state) {
-            case IDLE_STATE:
-                current_state = handle_idle_state();
-                break;
+            _delay_ms(100);
+            if(current_state == FAIL_STATE) {
+                handle_fail_state();
+            }
+            else if(current_state == WORKING_STATE) {
+                handle_working_state();
+            }
+            else {
+                handle_idle_state();
+            }
 
-            case FAIL_STATE:
-                current_state = handle_fail_state();
-                break;
 
-            case WORKING_STATE:
-                current_state = handle_working_state();
-                break;
         }
-    }
 
 }
